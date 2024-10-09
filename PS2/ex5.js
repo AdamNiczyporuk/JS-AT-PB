5911a262ed7de8f9a105b777b10a85b2d9b9a111
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-09 22:31:48 +0200

    End of ex5

--- PS2/ex5.js
+++ PS2/ex5.js
@@ -2,8 +2,34 @@ let data = "jajka, mleko, masło, chleb"
 
 let array = []
 
-let obejct = {}
+
 
 let products = data.split(",")
 
 console.log(products)
+
+products.forEach((product) => {
+    let price = Math.floor(Math.random() * 10 +1 ).toFixed(2)
+    array.push({ product: product.trim() , price: price })
+})
+
+console.table(array)
+
+let shoppingCart = []
+let maxItems = Math.floor(array.length / 2)
+
+for(let i=0; i<maxItems; i++)
+{
+    let random=Math.floor(Math.random() * array.length)
+    let item = array[random]
+    let quantity = Math.floor(Math.random() * 5 )+1
+    shoppingCart.push({product: item.product,price: item.price, quantity: quantity})
+    array.splice(random,1)
+}
+
+let totalPrice = shoppingCart.reduce((total, item) => {
+    return total + (item.price * item.quantity);
+}, 0);
+
+console.table(shoppingCart);
+console.log(totalPrice.toFixed(2));
