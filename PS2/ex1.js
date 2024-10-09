1ef0cdf8f0f2664c7a4514ccc91ca586865ab6e0
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-09 20:00:52 +0200

    Add function to js

---
+++ PS2/ex1.js
@@ -0,0 +1,12 @@
+const wejscie = document.getElementById("wejscie")
+const wyjscie = document.getElementById("wyjscie")
+const przycisk = document.getElementById("przycisk")
+
+przycisk.onclick = function() {
+  let wartosc = wejscie.value;
+  let liczba = parseInt(wartosc,10);
+  if(Number.isInteger(liczba))
+  { 
+    
+  }
+} 
