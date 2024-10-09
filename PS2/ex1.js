4285ccf7ed244652db2819ceb855e9167f1e0067
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-09 20:12:19 +0200

    Ex1 Done

--- PS2/ex1.js
+++ PS2/ex1.js
@@ -3,10 +3,31 @@ const wyjscie = document.getElementById("wyjscie")
 const przycisk = document.getElementById("przycisk")
 
 przycisk.onclick = function() {
-  let wartosc = wejscie.value;
+  let wartosc = wejscie.value
   let liczba = parseInt(wartosc,10);
   if(Number.isInteger(liczba))
   { 
     
+      if(wartosc%3 == 0)
+      { 
+        wyjscie.innerHTML = "Fiz"
+      }
+      else if(wartosc%2 == 0)
+      { 
+        wyjscie.innerHTML="Buz"
+      }
+      else if(wartosc%2== 0 && wartosc%3 ==0)
+      {
+        wyjscie.innerHTML("FizBuz")
+      }
+      else
+      { 
+        wyjscie.innerHTML = wartosc
+      }
+
+  }
+  else
+  { 
+    wyjscie.innerHTML = "Wpisz Liczbę"
   }
 } 
