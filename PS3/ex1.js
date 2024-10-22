e2fe455d423725107e20d40b215de7ed5e391e72
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-22 13:19:07 +0200

    First  subsection done ex1

--- PS3/ex1.js
+++ PS3/ex1.js
@@ -1,7 +1,17 @@
-function combion(operation,array1,array2)
+function combine(operation,array1,array2)
 {
     if( array1.length !== array2.length)
     {
         throw new Error('Arrays must have the same length');
     }
+
+    let result = [];
+
+    for(let i = 0; i < array1.length; i++)
+    {
+        result.push(operation(array1[i],array2[i]));
+    }
+    return result;
 }
+let wynik=combine((a,b)=>a+b,[4,5,6],[10,20,30]);
+console.log(wynik);
