69494810d043102fc51cf1075e21f64147b69a39
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-07 11:25:21 +0100

    Comment Uselees Data

--- PS5/ex1.js
+++ PS5/ex1.js
@@ -9,15 +9,17 @@ function  GetRandomColor()
 }
 
 
-document.getElementById("paragraf1").style.color =  GetRandomColor()
-document.getElementById("paragraf2").style.color = GetRandomColor()
-document.getElementById("paragraf3").style.color = GetRandomColor()
-document.getElementById("paragraf4").style.color = GetRandomColor()
+// document.getElementById("paragraf1").style.color =  GetRandomColor()
+// document.getElementById("paragraf2").style.color = GetRandomColor()
+// document.getElementById("paragraf3").style.color = GetRandomColor()
+// document.getElementById("paragraf4").style.color = GetRandomColor()
 
 
 const paragraph = document.querySelectorAll("p")
 
 paragraph.forEach((p) => {
     p.style.color = GetRandomColor()
+
     
+
 })
