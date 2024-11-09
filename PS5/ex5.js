32edbbb668d622d961bdbdc1fae29638014794dd
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-09 10:38:15 +0100

    Trying animation using js

--- PS5/ex5.js
+++ PS5/ex5.js
@@ -8,14 +8,57 @@ paragraphs.forEach((paragraph,index) => {
     paragraph.parentNode.insertBefore(header, paragraph);
 
 
-    header.addEventListener("click", () => { 
+    paragraph.style.height = paragraph.scrollHeight + "px";
+    paragraph.style.opacity = 1;
+
+    header.addEventListener("click", () => {
+        if (paragraph.style.height === "0px" || paragraph.style.opacity === "0") {
+            showParagraph(paragraph);
+        } else {
+            hideParagraph(paragraph);
+        }
+
+    // header.addEventListener("click", () => { 
         
-        if(paragraph.style.display === "none"){
-            paragraph.style.display = "block";
-        }
-        else
-        { 
-            paragraph.style.display = "none";
-        }
-    })
+    //     if(paragraph.style.display === "none"){
+    //         paragraph.style.display = "block";
+    //     }
+    //     else
+    //     { 
+    //         paragraph.style.display = "none";
+    //     }
+     })
 });
+
+function hideParagraph(paragraph)
+{ 
+    let height =paragraph.scrollHeight;
+    paragraph.style.transition = "opacity 0.5s ease";
+    paragraph.style.opacity = 1;
+    
+    const animate = setInterval(() => {
+        height -= 5; // Zmniejszamy wysokość stopniowo
+        paragraph.style.height = height + "px";
+        if (height <= 0) {
+            paragraph.style.opacity = 0;
+            paragraph.style.height = "0";
+            clearInterval(animate); // Kończymy animację
+        }
+    }, 10);
+}
+
+function showParagraph(paragraph) {
+    let height = 0;
+    const targetHeight = paragraph.scrollHeight;
+    paragraph.style.transition = "opacity 0.5s ease";
+    paragraph.style.opacity = 1;
+
+    const animate = setInterval(() => {
+        height += 5; // Zwiększamy wysokość stopniowo
+        paragraph.style.height = height + "px";
+        if (height >= targetHeight) {
+            paragraph.style.height = ""; // Usuwamy styl inline po zakończeniu
+            clearInterval(animate);
+        }
+    }, 10);
+}
