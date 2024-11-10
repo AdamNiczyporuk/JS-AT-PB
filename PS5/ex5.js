a22e1026fdcc711f37b855ac7cc7215460c71a91
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-10 17:08:16 +0100

    Edit html  and fix adding paragraph

--- PS5/ex5.js
+++ PS5/ex5.js
@@ -25,14 +25,31 @@ const paragraph = document.querySelectorAll('p')
 
 document.getElementById("addParagraph").addEventListener("click",() => { 
     const input = document.getElementById("newParagraphText"); 
-    const text = input.vale.trim();
+    const text = input.value.trim();
 
     if(text)
     { 
         const newParagraph = document.createElement('p')
         newParagraph.textContent = text;
-        document.body.appendChild(newParagraph0); 
+        document.body.appendChild(newParagraph); 
 
-        const allParagraph
+        // const allParagraphs = document.querySelector('p')
+        input = "";
     }
 })
+
+// document.getElementById("addParagraph").addEventListener("click", () => {
+//     const input = document.getElementById("newParagraphText");
+//     const text = input.value.trim();
+
+//     if (text) {
+//         const newParagraph = document.createElement("p");
+//         newParagraph.textContent = text;
+//         document.body.appendChild(newParagraph);
+
+//         const allParagraphs = document.querySelectorAll("p");
+//         addHeaderAndEvent(newParagraph, allParagraphs.length - 1);
+
+//         input.value = ""; // Czyszczenie pola tekstowego
+//     }
+// });
