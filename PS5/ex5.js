411f4309387791a785b818866bda23a19c1f2860
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-10 17:13:42 +0100

    Add function taht give styles headers to paragraphs

--- PS5/ex5.js
+++ PS5/ex5.js
@@ -1,27 +1,23 @@
-const paragraphs = document.querySelectorAll("p");
+function AddEventToParagraph(paragraph,index)
+{ 
+    
+        const header = document.createElement("h2");
+        header.textContent = `Paragraph ${index + 1}`;
+    
+        paragraph.parentNode.insertBefore(header, paragraph);
+    
+        paragraph.style.display= "block";
+
+        header.addEventListener("click",() =>{ 
+            paragraph.style.display= paragraph.style.display === "none" ? "block" : "none";
+        })
+    
+}
 
 
-paragraphs.forEach((paragraph,index) => {
-    const header = document.createElement("h2");
-    header.textContent = `Paragraph ${index + 1}`;
 
-    paragraph.parentNode.insertBefore(header, paragraph);
-
-
-   
-
-    header.addEventListener("click", () => { 
-        
-        if(paragraph.style.display === "none"){
-            paragraph.style.display = "block";
-        }
-        else
-        { 
-            paragraph.style.display = "none";
-        }
-     })
-});
 const paragraph = document.querySelectorAll('p')
+
 
 document.getElementById("addParagraph").addEventListener("click",() => { 
     const input = document.getElementById("newParagraphText"); 
@@ -33,23 +29,9 @@ document.getElementById("addParagraph").addEventListener("click",() => {
         newParagraph.textContent = text;
         document.body.appendChild(newParagraph); 
 
-        // const allParagraphs = document.querySelector('p')
+        const allParagraphs = document.querySelector('p')
+        AddEventToParagraph(newParagraph,allParagraphs-1)
         input = "";
     }
 })
 
-// document.getElementById("addParagraph").addEventListener("click", () => {
-//     const input = document.getElementById("newParagraphText");
-//     const text = input.value.trim();
-
-//     if (text) {
-//         const newParagraph = document.createElement("p");
-//         newParagraph.textContent = text;
-//         document.body.appendChild(newParagraph);
-
-//         const allParagraphs = document.querySelectorAll("p");
-//         addHeaderAndEvent(newParagraph, allParagraphs.length - 1);
-
-//         input.value = ""; // Czyszczenie pola tekstowego
-//     }
-// });
